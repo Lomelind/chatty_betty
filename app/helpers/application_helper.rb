6d89eac618ef8aafdc45aa8b9db371bf6693810c
Lomelind <valerolder@gmail.com>
2015-07-04 20:27:50 +0300

    Add static pages and ugly footer/ tests green

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def full_title(title = '')
+		base = "BettyChat"
+		if title.empty?
+			base
+		else
+			title
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
 
-  get 'static_pages/about'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get "about" => 'static_pages#about'
   # You can have the root of your site routed with "root"
    root 'static_pages#home'
 
